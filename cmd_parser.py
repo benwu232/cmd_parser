99b7938304c600d685061d3904a756ac381be781
benwu232 <benwu232@gmail.com>
2019-11-10 11:54:27 +0800

    refactoring the code, add docs or commands

--- cmd_parser.py
+++ cmd_parser.py
@@ -2,11 +2,6 @@ import sys
 
 # dict1=-ds ar=-ls 10 11 12 dict2=-ds x=1 y=2 seq=-ls 7 8 seq2=-ls 9 -le -le -de 13 -le a=1 c=2 dict3=-ds f=3 h=i -de j=k -de
 
-#example
-# n: 100 dict1: { m: 3 ar: [ 10 11 12 { x: 1 y: 2 seq: [ 7 8 [ 9 ] ] } 13 ] a: 1 c: 2 dict3: { f: 3 h: i } j: k }
-# The gramma is similar to python
-# { } contains directory
-# [ ] contains list
 
 def parse_dict(str_in):
     dic = {}
@@ -28,15 +23,28 @@ def parse_dict(str_in):
 
 
 def parse_cmd(cmd_list):
+    '''
+    Parse command line to python directory
+    The gramma is similar to python
+    { } contains directory
+    [ ] contains list
+
+    example
+    n: 100 dict1: { m: 3 ar: [ 10 11 12 { x: 1 y: 2 seq: [ 7 8 [ 9 ] ] } 13 ] a: 1 c: 2 dict3: { f: 3 h: i } j: k }
+    out_dict {'n': '100', 'dict1': {'m': '3', 'ar': ['10', '11', '12', {'x': '1', 'y': '2', 'seq': ['7', '8', ['9']]}, '13'], 'a': '1', 'c': '2', 'dict3': {'f': '3', 'h': 'i'}, 'j': 'k'}}
+
+    :param cmd_list:
+    :return: python directory which contains command list
+    '''
     cmd_dict = {}
-    layers = [cmd_dict]
+    cmd_stacks = [cmd_dict]
     cur_key = None
 
-    def pro(section):
-        if type(layers[-1]) == list:
-            layers[-1].append(section)
-        elif type(layers[-1]) == dict:
-            layers[-1][cur_key] = section
+    def pro(seg):
+        if type(cmd_stacks[-1]) == list:
+            cmd_stacks[-1].append(seg)
+        elif type(cmd_stacks[-1]) == dict:
+            cmd_stacks[-1][cur_key] = seg
 
     for s in cmd_list:
         item = s
@@ -44,17 +52,17 @@ def parse_cmd(cmd_list):
             cur_key = s.split(':')[0]
 
         elif item == '{':
-            d = {}
-            pro(d)
-            layers.append(d)
+            seg = {}
+            pro(seg)
+            cmd_stacks.append(seg)
 
         elif item == '[':
-            d = []
-            pro(d)
-            layers.append(d)
+            seg = []
+            pro(seg)
+            cmd_stacks.append(seg)
 
         elif ']' in item or '}' in item:
-            layers.pop()
+            cmd_stacks.pop()
 
         else:
             pro(item)
@@ -64,8 +72,9 @@ def parse_cmd(cmd_list):
 
 if __name__ in '__main__':
     args = sys.argv[1:]
-    #args = in_str
     print('input', args)
     out_dict = parse_cmd(args)
     print('out_dict', out_dict)
     print('bingo')
+
+
